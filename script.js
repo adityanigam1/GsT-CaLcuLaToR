b18031e074ef191ae7291ca9d4dab339e5959ac8
adityanigam1 <71330736+adityanigam1@users.noreply.github.com>
2020-10-22 17:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 function calculate() {
     var a = document.getElementById('origAmt').value;
-
     var b = document.getElementById('gstAmt').value;
     var get = (a * b) / 100;
     var geta = +(a) + +(get);
     document.getElementById("demo").innerHTML = a;
     document.getElementById("demo1").innerHTML = get;
-    document.getElementById("demo2").innerHTML = geta;
-
-
-    console.log(geta);
+    document.getElementById("demo2").innerHTML = geta;  
 }
 
 function calculate1() {
     var a1 = document.getElementById('origAmt1').value;
-
     var b1 = document.getElementById('gstAmt1').value;
     var get1 = a1 * (100 / (100 + (+b1)));
-    var geta1 =  a1 - get1;
-    document.getElementById("1demo").innerHTML = a1;
-    document.getElementById("1demo1").innerHTML = get1;
-    document.getElementById("1demo2").innerHTML = geta1;
-
-
+    var geta1 = a1 - get1;
+    document.getElementById("total1").innerHTML = a1;
+    document.getElementById("total2").innerHTML = get1;
+    document.getElementById("total3").innerHTML = geta1;
 }
 
 function MyCal(ls_from) {
@@ -30,11 +23,9 @@ function MyCal(ls_from) {
     var ld_per = document.getElementById("input_per").value;
     var ld_amt = document.getElementById("input_gst").value;
 
-
     if (ls_from == 'A') {
         ld_pri = (+(ld_amt) * 100) / +(ld_per);
-        document.getElementById("input_pri").value = ld_pri;
-
+        document.getElementById("input_pri").value = ld_pri;   
     }
     else if (ls_from == 'B') {
         ld_per = (+(ld_amt) * 100) / +(ld_pri);
@@ -44,6 +35,5 @@ function MyCal(ls_from) {
         ld_amt = (+(ld_pri) * +(ld_per) / 100);
         document.getElementById("input_gst").value = ld_amt;
     }
-
 }
 
